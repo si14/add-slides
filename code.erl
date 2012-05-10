eadaa5530022c3584d48f0a36dfb199d7862a8a6
Dmitry Groshev <groshev@selectel.ru>
2012-05-10 14:03:45 +0400

    next version

--- code.erl
+++ code.erl
@@ -25,3 +25,61 @@ handle(Data) ->
     {ok, FooResult} = foo(Data),
     {ok, BarResult} = bar(FooResult),
     baz(BarResult).
+
+handle(Req, #state{...}=State) ->
+    {Bindings, Req@} = cowboy_http_req:bindings(Req),
+    {QSVals, Req@} = cowboy_http_req:qs_vals(Req@),
+    Req@ = try
+               {Method, TaskName, VarSpecs} =
+                   ?Z_CATCH({_, _, _} = lists:keyfind(Method, 1, TaskSpecs),
+                            bad_method),
+               TaskVarsRoute =
+                   ?Z_CATCH([fetch_var(RouteVar, RouteVarType, Bindings)
+                             || {RouteVar, RouteVarType} <- RouteVars],
+                            bad_route),
+               TaskVars = [?Z_CATCH(fetch_var(Var, VarType, QSVals),
+                                    {bad_var, Var})
+                           || {Var, VarType} <- VarSpecs],
+               z_return(rnbwdash_task:create(...))
+           catch
+               ?Z_OK(Task) -> form_reply(run_task(Task), Errors, Req@);
+               ?Z_ERROR(Err) -> form_error(Err, Req@)
+           end,
+    {ok, Req@, State}.
+
+try
+    {Method, TaskName, VarSpecs} =
+      ?Z_CATCH({_, _, _} = lists:keyfind(Method, 1, TaskSpecs),
+               bad_method),
+    TaskVarsRoute =
+      ?Z_CATCH([fetch_var(RouteVar, RouteVarType, Bindings)
+                || {RouteVar, RouteVarType} <- RouteVars],
+               bad_route),
+    TaskVars = [?Z_CATCH(fetch_var(Var, VarType, QSVals),
+                         {bad_var, Var})
+                || {Var, VarType} <- VarSpecs],
+    z_return(rnbwdash_task:create(...))
+catch
+    ?Z_OK(Task) -> form_reply(run_task(Task), Errors, Req@);
+    ?Z_ERROR(Err) -> form_error(Err, Req@)
+end
+
+-define(Z_CATCH(EXPR, ERROR),
+        try
+            EXPR
+        catch
+            _:_ -> throw({z_throw, {error, ERROR}})
+        end).
+
+{Method, TaskName, VarSpecs} =
+  ?Z_CATCH({_, _, _} = lists:keyfind(Method, 1, TaskSpecs)
+           bad_method)
+
+TaskVarsRoute =
+  ?Z_CATCH([fetch_var(RouteVar, RouteVarType, Bindings)
+            || {RouteVar, RouteVarType} <- RouteVars],
+           bad_route)
+
+TaskVars = [?Z_CATCH(fetch_var(Var, VarType, QSVals),
+                     {bad_var, Var})
+            || {Var, VarType} <- VarSpecs]
--- code.java
+++ code.java
@@ -1,4 +1,4 @@
-public class AddNumbers{
+public class AddNumbers {
     public static int add() {
         int a = 1;
         String b = "b";
@@ -6,10 +6,18 @@ public class AddNumbers{
     }
 }
 
-public class FindMean{
+public class FindMean {
     public static float mean(String[] args) {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         return (a + b) / 2;
+    }
+}
+
+public class test {
+    public static int add() throws IOException {
+        int a = 1;
+        int b = 1;
+        return a + b;
     }
 }
